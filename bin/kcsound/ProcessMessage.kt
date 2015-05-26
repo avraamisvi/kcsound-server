0421e973acc365f312012d39ca0224e87aba96b9
abraao.isvi <abraao.isvi@gmail.com>
2015-05-26 18:50:57 -0300

    parse message

--- bin/kcsound/ProcessMessage.kt
+++ bin/kcsound/ProcessMessage.kt
@@ -2,15 +2,18 @@ package kcsound
 
 import com.google.gson.Gson
 
-public data class Message(val data: String, val type: Int)
+public class Message {
+	var data: String?=null;
+    var type: Int?=null;
+}
 
 public class ProcessMessage {
 	
 	fun process(msg: String): Message {
 		val gson = Gson();
 		
-		val ret: Message = gson.fromJson(msg, Message::class);
+		val ret: Message = gson.fromJson(msg, javaClass<Message>());
 		
-		return Message("",1);
+		return ret;
 	}
 }
