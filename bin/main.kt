478f35e0ba29b45685eae2891ff2210ebd8472e3
abraao.isvi <abraao.isvi@gmail.com>
2015-05-27 20:18:01 -0300

    basic

--- bin/main.kt
+++ bin/main.kt
@@ -4,8 +4,32 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft;
 import org.java_websocket.drafts.Draft_10;
 import org.java_websocket.handshake.ServerHandshake;
+import javax.swing.JFrame
+import javax.swing.JButton
+import java.awt.event.ActionListener
+import java.awt.event.ActionListener
+import java.awt.event.ActionEvent
 
 fun main(args : Array<String>) {
+	
+	val frame: JFrame = JFrame();
 	val client = EmptyClient(URI("ws://localhost:8887"), Draft_10());
 	client.connect();
+	
+	frame.setSize(400, 400);
+	val btnStop = JButton("Stop");
+	
+	btnStop.addActionListener(ActionListener(){
+	    fun actionPerformed(e: ActionEvent) {
+	        
+	    }		
+	})
+	
+	frame.getContentPane().add(btnStop);
+	frame.pack();
+	
+
+	
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.show()
 }
