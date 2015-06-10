93c75810dead7a925cf7d539366232fc31732086
abraaoisvi <abraao.isvi@gmail.com>
2015-06-10 09:34:21 -0300

    fixing tests

--- src/kcsound/templates/OrchestraTemplate.kt
+++ src/kcsound/templates/OrchestraTemplate.kt
@@ -4,13 +4,7 @@ import kcsound.composition.*;
 
 public class OrchestraTemplate {
 
-  var header: String=
-                  """
-                  sr = 44100
-                  ksmps = 32
-                  nchnls = 2
-                  0dbfs  = 1
-                  """;
+  var header: String="sr = 44100\nksmps = 32\nnchnls = 2\n0dbfs  = 1\n\n";
   var globals: String="";
   var instruments: String="";
 
@@ -20,7 +14,7 @@ public class OrchestraTemplate {
 
   public fun addInstrument(instrument: String, id: Int) {
 
-    instruments += "instr $id\n $instrument\n endin \n";
+    instruments += "\ninstr $id\n $instrument\n endin \n";
   }
 
   public fun generate(): String {
